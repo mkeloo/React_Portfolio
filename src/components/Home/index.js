3e6e8d7a6d66b4c4339049a30192f3be3abf813f
mkeloo <mkeloo@ufl.edu>
2022-08-06 14:44:19 -0400

    Built HomePage 1

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,25 @@
-// 23:19
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          lobodan
+          <br />
+          web developer
+        </h1>
+        <h2>Front-End Developer / JavaScript Expert</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
